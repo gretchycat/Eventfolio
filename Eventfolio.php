90250fa0785a8f0e6aa00fb6b29ebd8cc1a73bcc
Gretchen Maculo <gretchynm@gmail.com>
2025-05-26 12:40:33 -0500

    database and settings viewer

--- Eventfolio.php
+++ Eventfolio.php
@@ -34,7 +34,7 @@ if ($files)
 
 // Add a Settings link on the Plugins page
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
-    $settings_link = '<a href="' . admin_url('admin.php?page=eventfolio">Settings</a>';
+    $settings_link = '<a href="' . admin_url('admin.php?page=eventfolio">Settings</a>');
     array_unshift($links, $settings_link);
     return $links;
 });
