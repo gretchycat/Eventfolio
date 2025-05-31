c7523a8d47255f1ff33a79acfb224579ca91d540
Gretchen Maculo <gretchynm@gmail.com>
2025-05-31 13:49:03 -0500

    user permission settings page

--- includes/20-category-viewer.php
+++ includes/20-category-viewer.php
@@ -4,11 +4,11 @@ if (!function_exists('ef_category_viewer_row')) {
     {
         if (!$cat) return;
         ?>
-        <div class="ef-category-row ef-category-viewer">
-            <div class="ef-category-col ef-col-name"><?php echo esc_html($cat->name); ?></div>
-            <div class="ef-category-col ef-col-visibility"><?php echo esc_html(ucfirst($cat->visibility ?? '')); ?></div>
-            <div class="ef-category-col ef-col-description"><?php echo esc_html($cat->description); ?></div>
-            <div class="ef-category-col ef-col-actions">
+        <div class="ef-row ef-viewer">
+            <div class="ef-col ef-col-name"><?php echo esc_html($cat->name); ?></div>
+            <div class="ef-col ef-col-visibility"><?php echo esc_html(ucfirst($cat->visibility ?? '')); ?></div>
+            <div class="ef-col ef-col-description"><?php echo esc_html($cat->description); ?></div>
+            <div class="ef-col ef-col-actions">
                 <a href="?page=eventfolio_categories&edit=<?php echo intval($cat->id); ?>" class="ef-btn ef-btn-edit">Edit</a>&nbsp;
                 <a href="?page=eventfolio_categories&delete=<?php echo intval($cat->id); ?>" class="ef-btn ef-btn-delete" onclick="return confirm('Delete this category?');">Delete</a>
             </div>
